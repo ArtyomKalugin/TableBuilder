aaff9b958ff5bb3a12f2a818ff63616bce25a320
KaluginAA <KaluginAA@akbarsdigital.ru>
2023-03-21 21:26:34 +0300

    Added new base classes for table work

--- TableBuilder/Base/BaseTableViewCell.swift
+++ TableBuilder/Base/BaseTableViewCell.swift
@@ -2,23 +2,39 @@ import Foundation
 import UIKit
 
 /// Базовый класс ячейки таблицы
-public class BaseTableViewCell: UITableViewCell {
+public class BaseTableViewCell: ConfigurableCell {
+    public static var reuseIdentifier: String = ""
+    
+    public static var estimatedHeight: CGFloat?
+    
+    public static var defaultHeight: CGFloat?
+    
+    
+    public typealias CellData = BaseCellViewModel
+    
+    // MARK: - Life cycle
+    
+    init() {
+        addViews()
+        configureAppearance()
+        configureLayout()
+    }
     
     // MARK: - Methods
-    
+
     func addViews() {
         // Override
     }
-    
+
     func configureAppearance() {
         // Override
     }
-    
+
     func configureLayout() {
         // Override
     }
     
-    func configure() {
+    public func configure(with _: BaseCellViewModel) {
         // Override
     }
 }
--- TableBuilder/Sources/TableRow.swift
+++ TableBuilder/Sources/TableRow.swift
@@ -1,5 +1,53 @@
-import Foundation
+import UIKit
+
+public protocol Row  {
+    var reuseIdentifier: String { get }
+    var cellType: AnyClass { get }
+
+    var estimatedHeight: CGFloat? { get }
+    var defaultHeight: CGFloat? { get }
+    
+    var hashValue: Int { get }
+    
+    func configure(_ cell: UITableViewCell)
+}
 
 /// Класс строки таблицы
-open class TableRow {
+open class TableRow<CellType: ConfigurableCell>: Row where CellType: UITableViewCell {
+    
+    // MARK: - Properties
+
+    public let item: CellType.CellData
+    
+    open var hashValue: Int {
+        return ObjectIdentifier(self).hashValue
+    }
+    
+    open var reuseIdentifier: String {
+        return CellType.reuseIdentifier
+    }
+    
+    open var estimatedHeight: CGFloat? {
+        return CellType.estimatedHeight
+    }
+    
+    open var defaultHeight: CGFloat? {
+        return CellType.defaultHeight
+    }
+    
+    open var cellType: AnyClass {
+        return CellType.self as AnyClass
+    }
+    
+    // MARK: - Life cycle
+    
+    public init(item: CellType.CellData) {
+        self.item = item
+    }
+    
+    // MARK: - Methods
+    
+    open func configure(_ cell: UITableViewCell) {
+        (cell as? CellType)?.configure(with: item)
+    }
 }
